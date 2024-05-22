1a0843e79d232f47700208309ab4ab2da7b4ef8b
freezeburger <renaud.dubuis@decryptage.net>
2024-05-22 11:13:14 +0100

    Encapsulation

--- 3.env-arg/src/main.js
+++ 3.env-arg/src/main.js
@@ -1,6 +1,44 @@
 console.log('Main')
 
+/* 
 console.log(process.env.NODE_ENV)
-console.log(process.env.NODE_PORT)
+console.log(process.env.NODE_PORT) 
+*/
 
-console.log(process.argv)
+/* const args = process.argv.slice(2).reduce( (config, entry ) => {
+    const [key,value] = entry.split('=');
+    // if(value) return {...config, [key]:value} // Créer un objet intermédiaire n'est pas optimal
+    if(value) config[key]=value;
+    return config;
+}, {}); */
+
+/* 
+// Plus rapid que le reduce
+let args = {}
+const entries = process.argv.slice(2);
+for (const item of  entries ) {
+    const [key,value] = item.split('=');
+    if(value) args[key]= value;
+}
+ */
+//console.log(args)
+
+
+// Tous les objets javascript sont extensibles
+/* class Config extends Array{
+    constructor( processArgs = [] ){
+        if(!Array.isArray(processArgs)) throw 'Array Required'
+        super(...processArgs) // spread 
+    }
+    toObject(){
+        return this.reduce( (config, entry ) => {
+            const [key,value] = entry.split('=');
+            // if(value) return {...config, [key]:value} // Créer un objet intermédiaire n'est pas optimal
+            if(value) config[key]=value;
+            return config;
+        },{})
+    }
+}
+
+const args = (new Config(process.argv)).toObject();
+console.log(args) */
