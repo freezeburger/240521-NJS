b0a892ee8c7227e0724453311fbfd25776fbff8f
freezeburger <renaud.dubuis@decryptage.net>
2024-05-23 15:41:36 +0100

    Refactoring

--- 8.express/src/loggers/morgan.js
+++ 8.express/src/loggers/morgan.js
@@ -2,13 +2,15 @@ const morgan = require('morgan')
 const path = require('path');
 const fs = require('fs');
 
+const TEN_HOURS = 36000000;
 const PATH = '../../logs/'
 const LOG_PATH = path.join(__dirname, PATH)
 
+
+let  FILENAME = `${ (new Date()).toISOString()}-access.log`
 if (!fs.existsSync(LOG_PATH))fs.mkdirSync(LOG_PATH);
 
-
-const accessLogStream = fs.createWriteStream( LOG_PATH + 'access.log', { flags: 'a' })
+const accessLogStream = fs.createWriteStream( LOG_PATH + FILENAME, { flags: 'a' })
 
 const logger = morgan('combined',  { stream: accessLogStream });
 
--- 8.express/src/main.js
+++ 8.express/src/main.js
@@ -1,6 +1,5 @@
 console.log("Main");
 
-
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
